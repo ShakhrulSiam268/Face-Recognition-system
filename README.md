862f3f1fdf28e34b92331aea86579f2efe7dae95
ShakhrulSiam268 <shakhrulsiam268@gmail.com>
2022-08-22 15:20:34 +0600

    argparse added

--- README.md
+++ README.md
@@ -14,5 +14,15 @@ input size = `112x112`
 
 Distance Type = `cosine distance` 
 
+## Inference
+
+Inference on test image 
+
+`python main.py --im --path=./test/8.jpg`
+
+Inference on webcam
+
+`python main.py`
+
 ## Training pipeline
 
--- main.py
+++ main.py
@@ -9,17 +9,18 @@ scale = 0.3
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--image", default=False, action="store_true")
+    parser.add_argument("--im", default=False, action="store_true")
+    parser.add_argument('--path', type=str, default='./test/8.jpg', help='input image path')
     args = parser.parse_args()
 
-    if args.image:
-        process_image()
+    if args.im:
+        image_path = args.path
+        process_image(image_path)
     else:
         process_video()
 
 
-def process_image():
-    image_path = './test/8.jpg'
+def process_image(image_path):
     if os.path.isfile(image_path):
         frame = cv2.imread(image_path)
     else:
@@ -37,22 +38,15 @@ def process_video():
     url = 0
     cap = CustomVideoCapture(url)
     FC = FaceClass(threshold=0.7)
-    AM = AttendanceManagement()
-    if cap.isOpened():
-        while True:
-            frame = cap.read()
-            image, detected_names = FC.face_recognition(frame, scale)
-            AM.data_write(detected_names)
-            # image = cv2.resize(image, (0, 0), fx=0.6, fy=0.6)
-            image = cv2.resize(image, (1500, 900))
-            cv2.imshow('Face Recognition MIS', image)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        cv2.destroyAllWindows()
-    else:
-        print('Unable to connect camera')
+    while True:
+        frame = cap.read()
+        image, detected_names = FC.face_recognition(frame, scale)
+        image = cv2.resize(image, (1500, 900))
+        cv2.imshow('Face Recognition MIS', image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
     main()
-
